8fe106f69b017e38c562dc5c58d4f6095c6048fb
Roy Moger-Reischer <rmogerr2@gmail.com>
2026-08-24 20:59:43 -0700

    Add: remove-element - submission-19

---
+++ Data Structures & Algorithms/remove-element/submission-19.py
@@ -0,0 +1,28 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        valindices=[]; n = 0
+        while n < len(nums):
+            #for index, d in enumerate(nums):
+            #print(index)
+            if nums[n] == val:
+        #valindices.append(index)
+                for i in range(n, len(nums)-1):
+                    try:
+                        nums[i]=nums[i+1]
+                    except IndexError:
+                        pass
+                nums[len(nums)-1]="_"    
+                #print(n)
+                #print(nums)
+                
+            elif nums[n] != val:
+                n += 1
+                #print(n)
+                #print(nums)
+                continue
+
+        #print(n)
+        #print(nums)
+        
+        k = [thing for thing in nums if thing != "_"]
+        return len(k)
