9ca2c6316e54fe3e95787d8ed29bf8cd071ef72c
Roy Moger-Reischer <rmogerr2@gmail.com>
2026-08-30 20:14:13 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        lefts =["{","(","["]; rights = ["}",")","]"]
+        corrRL = {"}":"{",")":"(","]":"["}
+        x=0
+        toclose=list()
+        while x < len(s):
+            print()
+            if s[x] in lefts:
+                toclose.append(s[x])
+                x+=1
+            elif s[x] in rights:
+                if len(toclose) == 0:
+                    return False
+                elif corrRL[s[x]] == toclose[-1]:
+                    toclose.pop(-1)
+                    x+=1
+                else:
+                    return False
+            else:
+                return False
+        if len(toclose) == 0:
+            return True
+        else: return False
